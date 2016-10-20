fd40e23015d5d5c8301bebc03b5ae9afd3859d53
nedda76 <nedda.kaltcheva@gmail.com>
2016-10-20 15:13:22 +0200

    Initial commit.

---
+++ app/connectors/SSTTPConnector.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.WSHttp
+import model.{LiabilityResult}
+import play.api.Logger
+import uk.gov.hmrc.play.config.ServicesConfig
+import uk.gov.hmrc.play.http._
+
+import scala.concurrent.Future
+
+object SSTTPConnector extends ServicesConfig {
+
+  val http: HttpGet with HttpPost = WSHttp
+
+  def totalLiability(isEligible : Boolean) (implicit hc : HeaderCarrier) : Future[LiabilityResult] = {
+    Logger.info("Calling the GET Liability - :)")
+    http.GET[LiabilityResult](url(s"/ssttp/total-liability?eligible=" + isEligible))
+  }
+
+  private def url(path: String) = baseUrl("self-service-time-to-pay") + path
+}
+
---
+++ project/FrontendBuild.scala
@@ -0,0 +1,50 @@
+import sbt._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+
+object FrontendBuild extends Build with MicroService {
+
+  override val appName = "self-service-time-to-pay-frontend"
+
+  override lazy val plugins: Seq[Plugins] = Seq(
+    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
+  )
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+  import play.core.PlayVersion
+
+  val compile = Seq(
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "5.0.0",
+    "uk.gov.hmrc" %% "play-auditing" % "1.0.0",
+    "uk.gov.hmrc" %% "play-authorised-frontend" % "4.0.0",
+    "uk.gov.hmrc" %% "play-config" % "1.2.0",
+    "uk.gov.hmrc" %% "play-json-logger" % "2.1.1",
+    "uk.gov.hmrc" %% "play-health" % "1.1.0",
+    "uk.gov.hmrc" %% "govuk-template" % "4.0.0",
+    "uk.gov.hmrc" %% "play-ui" % "4.4.0"
+  )
+
+  abstract class TestDependencies(scope: String) {
+    lazy val test : Seq[ModuleID] = Seq(
+      "org.scalatest" %% "scalatest" % "2.2.4" % scope,
+      "org.pegdown" % "pegdown" % "1.4.2" % scope,
+      "org.jsoup" % "jsoup" % "1.7.3" % scope,
+      "uk.gov.hmrc" %% "hmrctest" % "1.4.0" % scope,
+      "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    )
+  }
+
+  object Test extends TestDependencies("test")
+  object IntegrationTest extends TestDependencies("it")
+
+  def apply() = compile ++ Test.test ++ IntegrationTest.test
+}
+
+
+
+
+
