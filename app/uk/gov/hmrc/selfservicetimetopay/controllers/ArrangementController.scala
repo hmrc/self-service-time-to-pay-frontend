77069c0d29e0ea4f3c9f90aa0ed2db5a3f3556cc
Mark Paxton <mark.paxton@digital.hmrc.gov.uk>
2016-11-24 16:38:32 +0000

    SSTTP-478 Fixing workflow links for direct debit confirmation

--- app/uk/gov/hmrc/selfservicetimetopay/controllers/ArrangementController.scala
+++ app/uk/gov/hmrc/selfservicetimetopay/controllers/ArrangementController.scala
@@ -76,7 +76,7 @@ object ArrangementController extends FrontendController {
     if(form.hasErrors) {
       Future.successful(Ok(direct_debit_form.render(form, request)))
     } else {
-      Future.successful(Redirect(routes.ArrangementController.applicationCompletePresent()))
+      Future.successful(Redirect(routes.ArrangementController.directDebitConfirmationPresent()))
     }
   }
 
@@ -89,10 +89,7 @@ object ArrangementController extends FrontendController {
   }
 
   def directDebitConfirmationSubmit:Action[AnyContent] = Action.async {implicit request =>
-    Future.successful(Ok(direct_debit_confirmation.render(
-      generatePaymentSchedules(BigDecimal("2000.00"), Some(BigDecimal("100.00"))).last,
-      arrangementDirectDebit, request))
-    )
+    Future.successful(Redirect(routes.ArrangementController.applicationCompletePresent()))
   }
 
   def applicationCompletePresent:Action[AnyContent] = Action.async {implicit request =>
