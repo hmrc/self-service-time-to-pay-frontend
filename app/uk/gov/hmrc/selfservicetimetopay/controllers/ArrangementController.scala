d999c2fd0d6ffcadd059d2bdf8b6a2391b6da273
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2016-12-02 16:38:41 +0000

    First iteration of loading and submitting the instalment summary page

--- app/uk/gov/hmrc/selfservicetimetopay/controllers/ArrangementController.scala
+++ app/uk/gov/hmrc/selfservicetimetopay/controllers/ArrangementController.scala
@@ -23,9 +23,11 @@ import uk.gov.hmrc.domain.SaUtr
 import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.selfservicetimetopay.config.TimeToPayController
 import uk.gov.hmrc.selfservicetimetopay.connectors._
+import uk.gov.hmrc.selfservicetimetopay.forms.ArrangementForm
 import uk.gov.hmrc.selfservicetimetopay.models._
 import uk.gov.hmrc.selfservicetimetopay.modelsFormat._
-import views.html.selfservicetimetopay.arrangement.application_complete
+import views.html.selfservicetimetopay.arrangement.{application_complete, instalment_plan_summary}
+import uk.gov.hmrc.selfservicetimetopay.controllerVariables._
 
 import scala.concurrent.Future
 import scala.concurrent.Future.successful
@@ -36,6 +38,31 @@ class ArrangementController(ddConnector: DirectDebitConnector,
   val cesa: String = "CESA"
   val paymentFrequency = "Monthly"
   val paymentCurrency = "GBP"
+
+  def getInstalmentSummary = Action.async { implicit request =>
+    sessionCache.get.map {
+      case Some(submission@TTPSubmission(Some(schedule), _, _, _, _, _, _)) =>
+        Ok(showInstalmentSummary(schedule, ArrangementForm.dayOfMonthForm, request))
+      case _ => throw new RuntimeException("No data found")
+    }
+  }
+
+  private val showInstalmentSummary = instalment_plan_summary.render _
+
+//  def submitDayOfMonth = Action.async { implicit request =>
+//    Future(
+//      ArrangementForm.dayOfMonthForm.bindFromRequest().fold(
+//        formWithErrors => BadRequest(routes.ArrangementController.getInstalmentSummary()),
+//        _ => Redirect(routes.ArrangementController.getInstalmentSummary()))
+//    )
+//  }
+
+  def submitInstalmentSchedule = Action.async { implicit request =>
+    Future(ArrangementForm.dayOfMonthForm.bindFromRequest().fold(
+      formWithErrors => BadRequest(showInstalmentSummary(generatePaymentSchedules(BigDecimal.exact("5000"), None).head, formWithErrors, request)),
+      _ => Redirect(routes.DirectDebitController.getDirectDebit()))
+    )
+  }
 
   def submit() = Action.async { implicit request =>
     sessionCache.get.flatMap {
