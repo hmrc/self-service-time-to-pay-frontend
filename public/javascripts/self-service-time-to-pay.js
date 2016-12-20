9fc9a6d757a3f6e656b93df6d00e901a7a145e5b
Mark Paxton <mark.paxton@digital.hmrc.gov.uk>
2016-12-20 18:18:22 +0000

    Added tracking to interest and debits breakdown, and to change day of month form

--- public/javascripts/self-service-time-to-pay.js
+++ public/javascripts/self-service-time-to-pay.js
@@ -1,28 +1,41 @@
-$("a.trackedOutboundLink").on("mouseup", function() {
-  ga("send", "event", "click-outgoing-link", $(this).attr("href"));
-});
+$(function() {
 
-$("a.trackedSignInSidebar").on("mouseup", function() {
-    ga("send", "event", "click-sidebar-signin", window.location.pathname );
-});
+    $("a.trackedOutboundLink").on("mouseup", function () {
+        ga("send", "event", "click-outgoing-link", $(this).attr("href"));
+    });
 
-$(".trackedPrintLink").on("mouseup", function() {
-    ga("send", "event", "click-print", window.location.pathname);
-});
+    $("a.trackedSignInSidebar").on("mouseup", function () {
+        ga("send", "event", "click-sidebar-signin", window.location.pathname);
+    });
 
-$("#calculatorInitialPaymentForm").on("submit", function() {
-    ga("send", "event", "calculator-payment-today-update", $(this).find("input[name=amount]").val());
-});
+    $(".trackedPrintLink").on("mouseup", function () {
+        ga("send", "event", "click-print", window.location.pathname);
+    });
+
+    $(".trackedDetails").on('toggle', function () {
+        var d = $(this),
+            state = (d.prop("open") === true) ? "open" : "close";
+        ga("send", "event", state, d.attr("id"));
+    })
+
+    $("#calculatorInitialPaymentForm").on("submit", function () {
+        ga("send", "event", "calculator-payment-today-update", $(this).find("input[name=amount]").val());
+    });
+
+    $("#arrangementPaymentDayForm").on("submit", function () {
+        ga("send", "event", "arrangement-day-of-month-change", $(this).find("input[name=dayOfMonth]").val());
+    });
 
 
-$("#calculatorDurationForm").on("submit", function() {
-    ga("send", "event", "calculator-duration-update", $(this).find("input[name=months]").val());
-});
+    $("#calculatorDurationForm").on("submit", function () {
+        ga("send", "event", "calculator-duration-update", $(this).find("input[name=months]").val());
+    });
 
-$("#calculatorDurationDecrease").on("mouseup", function() {
-    ga("send", "event", "calculator-duration-decrease-from", $("input[name=months]").val());
-});
+    $("#calculatorDurationDecrease").on("mouseup", function () {
+        ga("send", "event", "calculator-duration-decrease-from", $("input[name=months]").val());
+    });
 
-$("#calculatorDurationIncrease").on("mouseup", function() {
-    ga("send", "event", "calculator-duration-increase-from", $("input[name=months]").val());
+    $("#calculatorDurationIncrease").on("mouseup", function () {
+        ga("send", "event", "calculator-duration-increase-from", $("input[name=months]").val());
+    });
 });
