b8c6a00af5967767e02d92755c628efee38ace70
Mark Paxton <mark.paxton@digital.hmrc.gov.uk>
2016-12-20 12:52:34 +0000

    Removed back link tracking

--- public/javascripts/self-service-time-to-pay.js
+++ public/javascripts/self-service-time-to-pay.js
@@ -10,11 +10,6 @@ $(".trackedPrintLink").on("mouseup", function() {
     ga("send", "event", "click-print", window.location.pathname);
 });
 
-$(".trackedBackLink").on("mouseup", function() {
-    ga("send", "event", "clicked-back", window.location.pathname);
-});
-
-
 $("#calculatorInitialPaymentForm").on("submit", function() {
     ga("send", "event", "calculator-payment-today-update", $(this).find("input[name=amount]").val());
 });
