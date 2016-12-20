3428769208b8ef7efe8bec8c0c1f5fd270271c4c
Mark Paxton <MarkPaxton@users.noreply.github.com>
2016-12-20 09:13:57 +0000

    Ga integration (#103)
    * Fix assets url
    
    * Add some Google Analytics even notifications on calcualtor page for changing initial payment amount and duration, on sidebar links and print links and on going back

--- public/javascripts/self-service-time-to-pay.js
+++ public/javascripts/self-service-time-to-pay.js
@@ -1,1 +1,33 @@
+$("a.trackedOutboundLink").on("mouseup", function() {
+  ga("send", "event", "click-outgoing-link", $(this).attr("href"));
+});
 
+$("a.trackedSignInSidebar").on("mouseup", function() {
+    ga("send", "event", "click-sidebar-signin", $("h1, h2").first().text() );
+});
+
+$(".print-link").on("mouseup", function() {
+    ga("send", "event", "click-print", $("h1, h2").first().text());
+});
+
+$(".back-link").on("mouseup", function() {
+    ga("send", "event", "clicked-back", $("h1, h2").first().text());
+});
+
+
+$("#calculatorInitialPaymentForm").on("submit", function() {
+    ga("send", "event", "calculator-payment-today-update", $(this).find("input[name=amount]").val());
+});
+
+
+$("#calculatorDurationForm").on("submit", function() {
+    ga("send", "event", "calculator-duration-update", $(this).find("input[name=months]").val());
+});
+
+$("#calculatorDurationDecrease").on("mouseup", function() {
+    ga("send", "event", "calculator-duration-decrease-from", $("input[name=months]").val());
+});
+
+$("#calculatorDurationIncrease").on("mouseup", function() {
+    ga("send", "event", "calculator-duration-increase-from", $("input[name=months]").val());
+});
