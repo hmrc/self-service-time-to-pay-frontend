d21fe5dd0f771e6bcfe5753e5dead46bc1e39adf
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2017-01-01 20:14:32 +0000

    copyright auto updates

--- app/uk/gov/hmrc/selfservicetimetopay/models/CalculatorAmountsDue.scala
+++ app/uk/gov/hmrc/selfservicetimetopay/models/CalculatorAmountsDue.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,4 +20,3 @@ case class CalculatorAmountsDue(amountsDue: Seq[Debit]) {
 
   def total:BigDecimal = amountsDue.map(_.amount).sum
 }
-
