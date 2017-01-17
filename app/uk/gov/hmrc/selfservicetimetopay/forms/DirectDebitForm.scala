785f93f525cc5bcc32c18a9bfbc3b6698a6c2742
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2017-01-17 11:04:46 +0000

    updated dd form, messages, input validation length

--- app/uk/gov/hmrc/selfservicetimetopay/forms/DirectDebitForm.scala
+++ app/uk/gov/hmrc/selfservicetimetopay/forms/DirectDebitForm.scala
@@ -30,8 +30,7 @@ object DirectDebitForm {
 
   val directDebitMapping = mapping(
     "accountName" -> text.verifying("ssttp.direct-debit.form.error.accountName.required", _.trim!="")
-      .verifying("ssttp.direct-debit.form.error.accountName.not-text", x => x.length == x.replaceAll("[^a-zA-Z '.& \\/]", "").length)
-      .verifying("ssttp.direct-debit.form.error.accountName.length", _.trim.length < 40),
+      .verifying("ssttp.direct-debit.form.error.accountName.not-text", x => x.length == x.replaceAll("[^a-zA-Z '.& \\/]", "").length),
     "sortCode" -> text.verifying("ssttp.direct-debit.form.error.sortCode.required", _.trim!="")
       .verifying("ssttp.direct-debit.form.error.sortCode.not-number", x => (x.trim=="") || (x.replaceAll("[^0-9]", "")!="") || parseIntOption(x).nonEmpty)
       .verifying("ssttp.direct-debit.form.error.sortCode.not-valid", x=> (x.replaceAll("[^0-9]", "").length == 6) && x.length == 6),
