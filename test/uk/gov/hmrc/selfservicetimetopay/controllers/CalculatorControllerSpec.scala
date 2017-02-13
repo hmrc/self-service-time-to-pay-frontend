6c08a89ecd86572d160761c6ccac0f9f42db64ba
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2017-02-13 16:51:04 +0000

    Added tests for the amount due date page and updated package to add required submission for this page

--- test/uk/gov/hmrc/selfservicetimetopay/controllers/CalculatorControllerSpec.scala
+++ test/uk/gov/hmrc/selfservicetimetopay/controllers/CalculatorControllerSpec.scala
@@ -18,20 +18,20 @@ package uk.gov.hmrc.selfservicetimetopay.controllers
 
 import java.time.LocalDate
 
-import org.mockito.Matchers
 import org.mockito.Matchers.any
 import org.mockito.Mockito._
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mock.MockitoSugar
 import play.api.http.Status
+import play.api.i18n.Messages
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.cache.client.CacheMap
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
-import uk.gov.hmrc.selfservicetimetopay.connectors.{CalculatorConnector, EligibilityConnector, SessionCacheConnector}
+import uk.gov.hmrc.selfservicetimetopay.connectors.{CalculatorConnector, SessionCacheConnector}
 import uk.gov.hmrc.selfservicetimetopay.models.{Debit, EligibilityExistingTTP, EligibilityTypeOfTax}
 import uk.gov.hmrc.selfservicetimetopay.resources._
 
@@ -175,6 +175,111 @@ class CalculatorControllerSpec extends UnitSpec with MockitoSugar with ScalaFutu
         .withCookies(sessionProvider.createTtpCookie())))
 
       status(result) shouldBe Status.SEE_OTHER
+    }
+
+    "Successfully display the amount due date page" in {
+      implicit val hc = new HeaderCarrier
+
+      when(mockSessionCache.get(any(), any()))
+        .thenReturn(Future.successful(Some(ttpSubmissionNoAmounts)))
+
+      val result = await(controller.getDebitDate().apply(FakeRequest()
+        .withCookies(sessionProvider.createTtpCookie())))
+
+      bodyOf(result) should include(Messages("ssttp.calculator.form.what-you-owe-date.example"))
+    }
+
+    "Successfully redirect to the start page when missing submission data for amount due date page" in {
+      implicit val hc = new HeaderCarrier
+
+      when(mockSessionCache.get(any(), any()))
+        .thenReturn(Future.successful(Some(ttpSubmissionNLIEmpty)))
+
+      val result = await(controller.getDebitDate().apply(FakeRequest()
+        .withCookies(sessionProvider.createTtpCookie())))
+
+      status(result) shouldBe Status.SEE_OTHER
+      redirectLocation(result).get shouldBe routes.SelfServiceTimeToPayController.start().url
+    }
+
+    "Successfully submit the amount due date page with valid form data" in {
+      implicit val hc = new HeaderCarrier
+
+      when(mockSessionCache.get(any(), any()))
+        .thenReturn(Future.successful(Some(ttpSubmissionNoAmounts)))
+
+      val result = await(controller.submitDebitDate().apply(FakeRequest()
+        .withFormUrlEncodedBody("dueBy.dueByYear" -> "2017",
+          "dueBy.dueByMonth" -> "1",
+          "dueBy.dueByDay" -> "31")
+        .withCookies(sessionProvider.createTtpCookie())))
+
+      status(result) shouldBe Status.SEE_OTHER
+      redirectLocation(result).get shouldBe routes.CalculatorController.getAmountOwed().url
+    }
+
+    "Submit invalid year lower than minimum value in form data and return bad request for due date page" in {
+      implicit val hc = new HeaderCarrier
+
+      when(mockSessionCache.get(any(), any()))
+        .thenReturn(Future.successful(Some(ttpSubmissionNoAmounts)))
+
+      val result = await(controller.submitDebitDate().apply(FakeRequest()
+        .withFormUrlEncodedBody("dueBy.dueByYear" -> "1900",
+          "dueBy.dueByMonth" -> "1",
+          "dueBy.dueByDay" -> "31")
+        .withCookies(sessionProvider.createTtpCookie())))
+
+      status(result) shouldBe Status.BAD_REQUEST
+      bodyOf(result) should include(Messages("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-year-too-low"))
+    }
+
+    "Submit invalid year greater than maximum value in form data and return bad request for due date page" in {
+      implicit val hc = new HeaderCarrier
+
+      when(mockSessionCache.get(any(), any()))
+        .thenReturn(Future.successful(Some(ttpSubmissionNoAmounts)))
+
+      val result = await(controller.submitDebitDate().apply(FakeRequest()
+        .withFormUrlEncodedBody("dueBy.dueByYear" -> "2111",
+          "dueBy.dueByMonth" -> "1",
+          "dueBy.dueByDay" -> "31")
+        .withCookies(sessionProvider.createTtpCookie())))
+
+      status(result) shouldBe Status.BAD_REQUEST
+      bodyOf(result) should include(Messages("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-year-too-high"))
+    }
+
+    "Submit invalid month in form data and return bad request for due date page" in {
+      implicit val hc = new HeaderCarrier
+
+      when(mockSessionCache.get(any(), any()))
+        .thenReturn(Future.successful(Some(ttpSubmissionNoAmounts)))
+
+      val result = await(controller.submitDebitDate().apply(FakeRequest()
+        .withFormUrlEncodedBody("dueBy.dueByYear" -> "2017",
+          "dueBy.dueByMonth" -> "13",
+          "dueBy.dueByDay" -> "31")
+        .withCookies(sessionProvider.createTtpCookie())))
+
+      status(result) shouldBe Status.BAD_REQUEST
+      bodyOf(result) should include(Messages("ssttp.calculator.form.what-you-owe.due_by.not-valid-month"))
+    }
+
+    "Submit invalid day in form data and return bad request for due date page" in {
+      implicit val hc = new HeaderCarrier
+
+      when(mockSessionCache.get(any(), any()))
+        .thenReturn(Future.successful(Some(ttpSubmissionNoAmounts)))
+
+      val result = await(controller.submitDebitDate().apply(FakeRequest()
+        .withFormUrlEncodedBody("dueBy.dueByYear" -> "2017",
+          "dueBy.dueByMonth" -> "1",
+          "dueBy.dueByDay" -> "32")
+        .withCookies(sessionProvider.createTtpCookie())))
+
+      status(result) shouldBe Status.BAD_REQUEST
+      bodyOf(result) should include(Messages("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-day"))
     }
 
   }
