256fbcb87ba6ecdab3c7c7fc53a823f0b039297b
Colm Cavanagh <colm.m.cavanagh@gmail.com>
2017-03-28 15:11:27 +0100

    Colm/ssttp 1180/fix error messages (#257)
    * added extra check in for a decimal place within date fields
    
    * how much you owe form updated with latest error messages
    
    * Added in form error summary to payment today question
    
    * Updated how much would you like to pay today error messages
    
    * added missing apostrophe within error message
    
    * Made it so a letter will return the error message with a number
    
    * fixed date error messages so it will display correct error with a decimal point
    
    * Fixed multiple errors showing when more than 2 decimals are used in the amounts due form
    
    * Changed errors messages on pay today form
    
    * Changed would you like to pay today form to allow a blank value
    
    * removed unused commit
    
    * Fixed imports in Calculator Form

--- app/uk/gov/hmrc/selfservicetimetopay/forms/CalculatorForm.scala
+++ app/uk/gov/hmrc/selfservicetimetopay/forms/CalculatorForm.scala
@@ -18,13 +18,12 @@ package uk.gov.hmrc.selfservicetimetopay.forms
 
 import java.time.{DateTimeException, LocalDate}
 import java.util.Calendar
-import java.util.logging.Logger
 
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
 import uk.gov.hmrc.selfservicetimetopay.models.CalculatorAmountDue.MaxCurrencyValue
-import uk.gov.hmrc.selfservicetimetopay.models.{CalculatorPaymentToday, _}
+import uk.gov.hmrc.selfservicetimetopay.models._
 
 import scala.util.Try
 import scala.util.control.Exception.catching
@@ -47,21 +46,23 @@ object CalculatorForm {
 
   val dueByDateValidation = tuple(
     "dueByDay" -> text
-      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-day", { i: String => (i != null) && i.nonEmpty})
-      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-day-number", { i =>  i.isEmpty || (i.nonEmpty && isInt(i))})
-      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-day", { i: String => !i.contains(".")})
+      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-day", { i: String =>hasValue(i) & !i.contains(".")})
+      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-day-number", { i => i.contains(".") ||  i.isEmpty || (i.nonEmpty && isInt(i))})
        .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-day-number", { i => !isInt(i) || (isInt(i) && (i.toInt <= dueByDayMax) && (i.toInt >= dueByDayMin)) }),
     "dueByMonth" -> text
-      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-month", { i: String => ((i != null) && i.nonEmpty)})
-      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-month-number", { i => i.isEmpty || (i.nonEmpty && isInt(i)) })
-      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-month", { i: String => !i.contains(".")})
+      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-month", { i: String => hasValue(i) & !i.contains(".")})
+      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-month-number", { i =>i.contains(".") ||  i.isEmpty || (i.nonEmpty && isInt(i))})
       .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-month-number", { i => !isInt(i) || (isInt(i) && (i.toInt <= dueByMonthMax) && (i.toInt >= dueByMonthMin)) }),
     "dueByYear" -> text
-      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-year", { i: String => (i != null) && i.nonEmpty })
+      .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-year", { i: String =>hasValue(i) })
       .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-year", { i => i.isEmpty || (i.nonEmpty && isInt(i)) })
       .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-year-too-high", { i => !isInt(i) || (isInt(i) && (i.toInt <= dueByYearMax)) })
       .verifying("ssttp.calculator.form.what-you-owe-date.due_by.not-valid-year-too-low", { i => !isInt(i) || (isInt(i) && (i.toInt >= dueByYearMin)) })
   )
+  def hasValue(textBox:String):Boolean ={
+    (textBox != null) && textBox.nonEmpty
+  }
+
 
   def dateValidator: Constraint[(String, String, String)] =
     Constraint[(String, String, String)]("ssttp.calculator.form.what-you-owe.due_by.not-valid-date") { data =>
@@ -84,8 +85,8 @@ object CalculatorForm {
   def createPaymentTodayForm(totalDue: BigDecimal): Form[CalculatorPaymentToday] = {
     Form(mapping(
       "amount" -> text
+        .verifying("ssttp.calculator.form.payment_today.amount.required.min", { i: String =>  if (i.nonEmpty && Try(BigDecimal(i)).isSuccess &&  BigDecimal(i).scale <= 2 ) BigDecimal(i) >= 0.00 else true})
         .verifying("ssttp.calculator.form.payment_today.amount.required", { i => Try(BigDecimal(i)).isSuccess})
-        .verifying("ssttp.calculator.form.payment_today.amount.required.min", { i: String =>  if (i.nonEmpty && Try(BigDecimal(i)).isSuccess) BigDecimal(i) >= 0.01 else true})
         .verifying("ssttp.calculator.form.payment_today.amount.decimal-places", { i =>
           if (Try(BigDecimal(i)).isSuccess) BigDecimal(i).scale <= 2 else true})
         .verifying("ssttp.calculator.form.payment_today.amount.less-than-owed", i =>
@@ -110,7 +111,7 @@ object CalculatorForm {
     Form(mapping(
       "amount" -> text
         .verifying("ssttp.calculator.form.what-you-owe-amount.amount.required", { i: String =>  Try(BigDecimal(i)).isSuccess})
-        .verifying("ssttp.calculator.form.what-you-owe-amount.amount.required.min", { i: String =>  if (i.nonEmpty && Try(BigDecimal(i)).isSuccess) BigDecimal(i) >= 0.01 else true})
+        .verifying("ssttp.calculator.form.what-you-owe-amount.amount.required.min", { i: String =>  if (i.nonEmpty && Try(BigDecimal(i)).isSuccess && BigDecimal(i).scale <= 2) BigDecimal(i) >= 0.01 else true})
         .verifying("ssttp.calculator.form.what-you-owe-amount.amount.less-than-maxval", { i: String =>
           if (i.nonEmpty && Try(BigDecimal(i)).isSuccess) BigDecimal(i) < MaxCurrencyValue else true
         })
