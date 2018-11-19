40e1cc799bf8bd416e46690b990c496292ac34ee
cavster <colm.m.cavanagh@gmail.com>
2018-11-19 16:34:39 +0000

    [cc]Removed unused import

--- app/uk/gov/hmrc/selfservicetimetopay/controllers/SelfServiceTimeToPayController.scala
+++ app/uk/gov/hmrc/selfservicetimetopay/controllers/SelfServiceTimeToPayController.scala
@@ -20,7 +20,6 @@ import play.api.mvc._
 import uk.gov.hmrc.selfservicetimetopay.config.SsttpFrontendConfig
 import uk.gov.hmrc.selfservicetimetopay.models._
 import uk.gov.hmrc.selfservicetimetopay.modelsFormat._
-import views.html.selfservicetimetopay.arrangement.application_complete
 import views.html.selfservicetimetopay.core._
 
 class SelfServiceTimeToPayController @Inject() (val messagesApi: play.api.i18n.MessagesApi) extends TimeToPayController with play.api.i18n.I18nSupport {
