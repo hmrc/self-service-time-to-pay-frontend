aef7b4b145b8fccec27a5a2de9985aa23dec3012
Colm Cavanagh <colm.m.cavanagh@gmail.com>
2018-11-27 15:51:11 +0000

    Colm/somme (#347)
    * added ui changes
    
    * [CC]Changed validation messages (#345)
    
    * [CC][SSTTP2-645]New page for users who owe over 10 k (#344)

--- public/javascripts/urBanner.js
+++ public/javascripts/urBanner.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
     var cookieData = GOVUK.getCookie("UrBannerHide");
     if (cookieData == null) {
-        $("#ur-panel").show();
+        $("#ur-panel").css('display', 'flex');
     }
-    console.log("Hello world");
     $(".banner-panel__close").on("click", function (e) {
         e.preventDefault();
         GOVUK.setCookie("UrBannerHide", "suppress_for_all_services", 99999999999);
