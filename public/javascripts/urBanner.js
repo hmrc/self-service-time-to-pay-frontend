f78233768ad7b99d4a38c4d85adf45d3680a1425
somme <sommes.email@gmail.com>
2018-11-27 16:05:27 +0000

    tweaks

--- public/javascripts/urBanner.js
+++ public/javascripts/urBanner.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+    //Accessibility
+    var errorSummary =  $('#error-summary-display'),
+    $input = $('input:text')
+    //Error summary focus
+    if (errorSummary){ errorSummary.focus() }
+    $input.each( function(){
+        if($(this).closest('label').hasClass('form-field--error')){
+            $(this).attr('aria-invalid', true)
+        }else{
+            $(this).attr('aria-invalid', false)
+        }
+    });
+
+    GOVUK.details.init()
+
     var cookieData = GOVUK.getCookie("UrBannerHide");
     if (cookieData == null) {
         $("#ur-panel").css('display', 'flex');
