162ea50c1cf4bbe2bfdf4e91bd3423234e0d5d2a
Andy Hicks <455080+andy1138@users.noreply.github.com>
2021-12-22 08:32:07 +0000

    [OPS-7123] HOTFIX with locking duplicates

--- app/ssttparrangement/ArrangementController.scala
+++ app/ssttparrangement/ArrangementController.scala
@@ -29,6 +29,7 @@ import journey.Statuses.{FinishedApplicationSuccessful, InProgress}
 import journey.{Journey, JourneyService}
 import play.api.Logger
 import play.api.mvc._
+import play.modules.reactivemongo.ReactiveMongoComponent
 import playsession.PlaySessionSupport._
 import req.RequestSupport
 import ssttparrangement.ArrangementForm.dayOfMonthForm
@@ -39,7 +40,8 @@ import times.ClockProvider
 import ssttpcalculator.model.{Instalment, PaymentSchedule}
 import timetopaytaxpayer.cor.model.{SelfAssessmentDetails, Taxpayer}
 import timetopaytaxpayer.cor.{TaxpayerConnector, model}
-import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.{HeaderCarrier, SessionKeys}
+import uk.gov.hmrc.lock.{LockKeeper, LockMongoRepository, LockRepository}
 import uk.gov.hmrc.selfservicetimetopay.jlogger.JourneyLogger
 import uk.gov.hmrc.selfservicetimetopay.models._
 import views.Views
@@ -50,20 +52,21 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.math.BigDecimal.exact
 
 class ArrangementController @Inject() (
-    mcc:                  MessagesControllerComponents,
-    ddConnector:          DirectDebitConnector,
-    arrangementConnector: ArrangementConnector,
-    calculatorService:    CalculatorService,
-    eligibilityService:   EligibilityService,
-    taxPayerConnector:    TaxpayerConnector,
-    auditService:         AuditService,
-    journeyService:       JourneyService,
-    as:                   Actions,
-    requestSupport:       RequestSupport,
-    views:                Views,
-    clockProvider:        ClockProvider,
-    iaService:            IaService,
-    directDebitConnector: DirectDebitConnector)(
+    mcc:                    MessagesControllerComponents,
+    ddConnector:            DirectDebitConnector,
+    arrangementConnector:   ArrangementConnector,
+    calculatorService:      CalculatorService,
+    eligibilityService:     EligibilityService,
+    taxPayerConnector:      TaxpayerConnector,
+    auditService:           AuditService,
+    journeyService:         JourneyService,
+    as:                     Actions,
+    requestSupport:         RequestSupport,
+    views:                  Views,
+    clockProvider:          ClockProvider,
+    iaService:              IaService,
+    reactiveMongoComponent: ReactiveMongoComponent,
+    directDebitConnector:   DirectDebitConnector)(
     implicit
     appConfig: AppConfig,
     ec:        ExecutionContext) extends FrontendBaseController(mcc) {
@@ -217,12 +220,20 @@ class ArrangementController @Inject() (
     }
 
   def submit(): Action[AnyContent] = as.authorisedSaUser.async { implicit request =>
-    JourneyLogger.info(s"ArrangementController.submit: $request")
-    journeyService.authorizedForSsttp { ttp =>
-      ttp.requireScheduleIsDefined()
-      ttp.requireDdIsDefined()
-      val paymentSchedule = calculatorService.computeSchedule(ttp)
-      arrangementSetUp(ttp, paymentSchedule)
+    tryLock(request.request.session.get(SessionKeys.sessionId).getOrElse("unknown-session")) {
+      JourneyLogger.info(s"ArrangementController.submit: $request")
+      journeyService.authorizedForSsttp { ttp =>
+        ttp.requireScheduleIsDefined()
+        ttp.requireDdIsDefined()
+        val paymentSchedule = calculatorService.computeSchedule(ttp)
+        arrangementSetUp(ttp, paymentSchedule)
+      }
+    }.map {
+      case Some(res) =>
+        res
+      case err =>
+        JourneyLogger.info(s"ArrangementController.submit: locked, duplicate request: $request, err: $err")
+        Redirect(routes.ArrangementController.getTermsAndConditions())
     }
   }
 
@@ -360,6 +371,18 @@ class ArrangementController @Inject() (
     val schedule = calculatorService.computeSchedule(journey)
 
     TTPArrangement(ppReference, ddReference, taxpayer, journey.bankDetails, schedule)
+  }
+
+  private def tryLock[T](lockName: String)(body: => Future[T])(implicit ec: ExecutionContext): Future[Option[T]] = {
+    lazy val lockKeeper = new LockKeeper {
+      override def repo: LockRepository = LockMongoRepository(reactiveMongoComponent.mongoConnector.db)
+
+      override def lockId: String = s"des-lock-" + lockName
+
+      override val forceLockReleaseAfter: org.joda.time.Duration = org.joda.time.Duration.standardMinutes(5)
+    }
+
+    lockKeeper.tryLock(body)
   }
 
 }
