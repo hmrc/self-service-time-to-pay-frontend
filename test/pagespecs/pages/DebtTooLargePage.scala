8c830d2ee2d9be79fd56d2f2f624e5b4325b8905
Olabode Alarapon <15871275+akure4life@users.noreply.github.com>
2022-08-31 12:23:27 +0100

    [OA][OPS-8491] Update Opening Times on Set Up a Self Assessment Payment Plan fixed
    Signed-off-by: olabode <15871275+Akure4Life@users.noreply.github.com>

--- test/pagespecs/pages/DebtTooLargePage.scala
+++ test/pagespecs/pages/DebtTooLargePage.scala
@@ -71,7 +71,7 @@ class DebtTooLargePage(baseUrl: BaseUrl)(implicit webDriver: WebDriver) extends 
           |what you've done to try to pay the bill
           |if you can pay some of the bill now
           |Our opening times are:
-          |Monday to Friday: 8am to 4pm"""
+          |Monday to Friday: 8am to 6pm"""
           .stripMargin
 
       private val mainTextWelsh =
@@ -87,7 +87,7 @@ class DebtTooLargePage(baseUrl: BaseUrl)(implicit webDriver: WebDriver) extends 
           |beth rydych wedi’i wneud i geisio talu’r bil
           |a allwch dalu rhywfaint o’r bil nawr
           |Ein horiau agor yw:
-          |Dydd Llun i ddydd Gwener: 08:30 – 16:00
+          |Dydd Llun i ddydd Gwener: 08:30 – 18:00
         """.stripMargin
     }
 
