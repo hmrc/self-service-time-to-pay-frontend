bdbdf240b0fccd03a47ea1dd2f46ef7c45d03c38
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2022-10-13 10:24:23 +0100

    [KN][OPS-8373] Fix hmrc-mongo version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
 
     "com.beachape" %% "enumeratum" % "1.6.1",
     "uk.gov.hmrc" %% "time" % "3.25.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.66.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.73.0",
 
     "uk.gov.hmrc" %% "time-to-pay-taxpayer-cor" % "0.40.0",
 
