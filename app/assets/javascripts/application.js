71c4ea3134af1e76fee3f3cbb48cad50916feb7c
Olabode Alarapon <15871275+akure4life@users.noreply.github.com>
2022-10-21 14:57:32 +0100

    [OA][OPS-9049] <script> tags placemen fixed
    Signed-off-by: olabode <15871275+Akure4Life@users.noreply.github.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,10 @@
 
 window.GOVUKFrontend.initAll();
 window.HMRCFrontend.initAll();
+
+
+function uncheck(id){
+        document.getElementById(id).checked = false
+
+        if(id=="other"){document.getElementById("customDate").style.display = "none"} else {document.getElementById("customDate").style.display = "block"}
+    }
