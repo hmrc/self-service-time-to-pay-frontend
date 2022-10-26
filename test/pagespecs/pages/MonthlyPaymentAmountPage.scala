dc554ae1e028d615944f85ef261927c9dcc7a06a
tp10 <1626193+tp10@users.noreply.github.com>
2022-10-26 16:25:39 +0100

    Add Welsh translation - Conflict changes

--- test/pagespecs/pages/MonthlyPaymentAmountPage.scala
+++ test/pagespecs/pages/MonthlyPaymentAmountPage.scala
@@ -77,8 +77,9 @@ class MonthlyPaymentAmountPage(baseUrl: BaseUrl)(implicit webDriver: WebDriver) 
 
       // "How much can you pay upfront in Pound Sterling" has css class visually hidden but is still read by content scraper
       private def mainTextEnglish(lowerAmount: Int, upperAmount: Int) =
-        s"""Enter an amount between £${format(lowerAmount)} and £${format(upperAmount)}
-           |How much can you afford to pay each month?
+        s"""How much can you afford to pay each month?
+           |Enter an amount between £${format(lowerAmount)} and £${format(upperAmount)}
+           |How much can you pay monthly in Pound Sterling
            |£
            |Continue
         """.stripMargin
