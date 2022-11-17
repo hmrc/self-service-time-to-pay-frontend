d5d1d836d552ffd13103ad7b5e462dce6f7bf605
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2022-11-17 14:55:07 +0000

    [KN][OPS-8656] Hiphens and spaces allowed for sort code

--- app/ssttpdirectdebit/DirectDebitForm.scala
+++ app/ssttpdirectdebit/DirectDebitForm.scala
@@ -19,6 +19,7 @@ package ssttpdirectdebit
 import play.api.data.Forms._
 import play.api.data.{Form, FormError}
 import uk.gov.hmrc.selfservicetimetopay.models.ArrangementDirectDebit
+import uk.gov.hmrc.selfservicetimetopay.models.ArrangementDirectDebit.cleanSortCode
 
 object DirectDebitForm {
 
@@ -30,11 +31,30 @@ object DirectDebitForm {
       .verifying("ssttp.direct-debit.form.error.accountName.check", _.matches("^[a-zA-Z '.&/]{1,39}$")), // regex from API#1856
     "sortCode" -> text
       .verifying("ssttp.direct-debit.form.error.sortCode.required", _.trim != "")
-      .verifying("ssttp.direct-debit.form.error.sortCode.not-valid", x => condTrue(x.trim != "", x.matches("[0-9]{6}"))),
+      .verifying("ssttp.direct-debit.form.error.sortCode.not-valid", isValidSortCode _),
     "accountNumber" -> text.verifying("ssttp.direct-debit.form.error.accountNumber.required", _.trim != "")
       .verifying("ssttp.direct-debit.form.error.accountNumber.not-valid", x => (x.trim == "") | x.matches("[0-9]{6,8}"))
-  )({ case (name, sc, acctNo) => ArrangementDirectDebit(name, sc, acctNo) }
+  )({ case (name, sc, acctNo) => ArrangementDirectDebit(name, cleanSortCode(sc), acctNo) }
     )({ case arrangementDirectDebit => Some((arrangementDirectDebit.accountName, arrangementDirectDebit.sortCode, arrangementDirectDebit.accountNumber)) })
+
+  def isValidSortCode(sortCode: String): Boolean = {
+    hasValidSortCodeCharacters(sortCode) && has6Numbers(sortCode)
+  }
+
+  private def has6Numbers(sortCode: String): Boolean = validateNumberLength(sortCode, 6)
+
+  private def hasValidSortCodeCharacters(sortCode: String): Boolean = {
+    sortCode
+      .trim
+      .replaceAll("\\d", "")
+      .replaceAll("-", "")
+      .replaceAll(" ", "")
+      .isEmpty
+  }
+
+  def validateNumberLength(number: String, length: Int): Boolean = {
+    number.replaceAll("[^0-9]", "").length == length
+  }
 
   val directDebitForm = Form(directDebitMapping)
   val directDebitFormWithBankAccountError = directDebitForm.copy(errors = Seq(FormError(" ", "ssttp.direct-debit.form.bank-not-found-info")))
