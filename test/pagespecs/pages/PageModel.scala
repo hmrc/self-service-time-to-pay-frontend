bead6e9e836966d7a434f71bb43eb2f7c81ac3ac
Olabode Alarapon <15871275+akure4life@users.noreply.github.com>
2023-01-04 12:51:18 +0000

    [OA][OPS-9016] Language Toggle
    Signed-off-by: olabode <15871275+Akure4Life@users.noreply.github.com>

--- test/pagespecs/pages/PageModel.scala
+++ test/pagespecs/pages/PageModel.scala
@@ -87,8 +87,8 @@ abstract class BasePage(baseUrl: BaseUrl)(implicit webDriver: WebDriver) {
 
   def readPath(): String = new java.net.URL(webDriver.getCurrentUrl).getPath
 
-  def clickOnEnglishLink(): Unit = click on id("en-switch")
-  def clickOnWelshLink(): Unit = click on id("cy-switch")
+  def clickOnEnglishLink(): Unit = click on id("en")
+  def clickOnWelshLink(): Unit = click on id("cy")
 
   def clickOnContinue(): Unit = click on id("continue")
 
