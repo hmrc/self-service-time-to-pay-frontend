0da7ce6bd907298ad71f08ccca96506ce3c7715f
Kostas Ntanos <kostas@ntanos.co.uk>
2023-03-02 17:26:35 +0000

    [KN][OPS-9637] Missing comma (#737)

--- test/pagespecs/pages/GeneralCallUsPage.scala
+++ test/pagespecs/pages/GeneralCallUsPage.scala
@@ -64,7 +64,7 @@ class GeneralCallUsPage(baseUrl: BaseUrl)(implicit webDriver: WebDriver) extends
         """Please call us
           |You are not eligible to set up a payment plan online.
           |
-          |For further support you can contact us on 0300 123 1813 to speak to an adviser.
+          |For further support, you can contact us on 0300 123 1813 to speak to an adviser.
           |
           |Before you call, make sure you have:
           |information on any savings or investments you have
