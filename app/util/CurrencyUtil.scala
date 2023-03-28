b1155c7b3c81a5eef8e26328d92baeac49ee56d1
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2023-03-28 12:34:02 +0100

    [KN][OPS-10050] Add comment to explain regex

--- app/util/CurrencyUtil.scala
+++ app/util/CurrencyUtil.scala
@@ -18,6 +18,30 @@ package util
 
 object CurrencyUtil {
 
+  /*
+  The regex will match currency input with a complex format:
+  - Spaces anywhere preceding or following any of the input characters
+  - One optional £ symbol preceding any digits, but only if it is followed by at least one digit
+  - One optional hyphen before the £ symbol or one optional hyphen after the £ symbol, but only one of them at one time. Both will be considered valid negative inputs.
+  - Optional commas as thousands separators, but only in valid positions.
+  - Optional dot as decimal separator followed by unlimited digits.
+
+  e.g. will match:
+  123, 45 6
+  £ 1, 2 34,5 67.8 99
+    -   £ 10
+  £ - 10
+  -1 0
+  £.1
+
+  e.g will not match:
+  - £-10
+  - - 10
+  12,34.5
+  £
+  £ .
+   */
+
   def regex: String = "(?!.*-.*-)[ ]*(-)?[ ]*(\\£)?[ ]*(?=.*\\d)(-)?[ ]*((?:\\d\\s*){1,3}(?:[ ]*)(\\,(?:[ ]*)(?:\\d\\s*){3})*|(\\d+))?((?:[ ]*)\\.[ ]*\\d+(?:[ ]*\\d+)*[ ]*)?$"
 
   def cleanAmount(text: String): String = text
