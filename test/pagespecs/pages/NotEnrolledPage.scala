7f404d4d91091f36fc484322c3140039ad791f5f
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-06-07 08:49:37 +0100

    [AM][OPS-10503] Connect 'not enrolled' with no credentials to generic eligibility kick-out page (#813)
    * [AM][OPS-10503] connect generic eligibility kick out page to not eligible rotten credentials journey
    
    * [AM][OPS-10503] delete superseded not enrolled eligibility kickout page

--- test/pagespecs/pages/NotEnrolledPage.scala
+++ test/pagespecs/pages/NotEnrolledPage.scala
@@ -33,8 +33,8 @@ class NotEnrolledPage(baseUrl: BaseUrl)(implicit webDriver: WebDriver) extends B
   }
 
   def expectedHeadingContent(language: Language): String = language match {
-    case Languages.English => "Please call us"
-    case Languages.Welsh   => "Ffoniwch ni"
+    case Languages.English => "Call us about a payment plan"
+    case Languages.Welsh   => "Ffoniwch ni ynghylch cynllun talu"
   }
 
   object Expected {
@@ -59,35 +59,41 @@ class NotEnrolledPage(baseUrl: BaseUrl)(implicit webDriver: WebDriver) extends B
       }
 
       private val mainTextEnglish =
-        """Please call us
-          |You may still be able to set up a payment plan over the phone, but you are not eligible for an online payment plan.
-          |
-          |For further support, you can contact us on 0300 123 1813 to speak to an adviser.
-          |
+        """Call us about a payment plan
+          |You are not eligible to set up a Self Assessment payment plan online.
+          |Call us on 0300 123 1813 as you may be able to set up a plan over the phone.
+          |Our opening times are Monday to Friday, 8am to 6pm. We are closed on weekends and bank holidays.
+          |If you need extra support
+          |Find out the different ways to deal with HMRC if you need some help.
+          |You can also use Relay UK if you cannot hear or speak on the phone: dial 18001 then 0345 300 3900.
+          |If you are outside the UK: +44 2890 538 192.
           |Before you call, make sure you have:
+          |your 10-digit Unique Taxpayer Reference (UTR) number
           |information on any savings or investments you have
           |your bank details
-          |We’re likely to ask:
-          |what you’ve done to try to pay the bill
+          |details of your income and spending
+          |We're likely to ask:
+          |what you've done to try to pay the bill
           |if you can pay some of the bill now
-          |Our opening times are:
-          |Monday to Friday: 8am to 6pm"""
-          .stripMargin
+        """.stripMargin
 
       private val mainTextWelsh =
-        """Ffoniwch ni
-          |Efallai y gallwch drefnu cynllun talu dros y ffôn, ond nid ydych yn gymwys i gael cynllun talu ar-lein.
-          |
-          |Rydym yn argymell eich bod yn siarad ag ymgynghorydd am eich opsiynau talu. Ffoniwch Wasanaeth Cwsmeriaid Cymraeg CThEF ar 0300 200 1900.
-          |
+        """Ffoniwch ni ynghylch cynllun talu
+          |Nid ydych yn gymwys i drefnu cynllun talu Hunanasesiad ar-lein.
+          |Ffoniwch ni ar 0300 200 1900 oherwydd mae’n bosibl y gallwch drefnu cynllun dros y ffôn.
+          |Ein horiau agor yw Dydd Llun i Ddydd Gwener, 08:30 i 17:00. Rydym ar gau ar benwythnosau a gwyliau banc.
+          |Os oes angen cymorth ychwanegol arnoch chi
+          |Dysgwch am y ffyrdd gwahanol o ddelio â CThEF os oes angen help arnoch chi.
+          |Gallwch hefyd ddefnyddio Relay UK os na allwch glywed na siarad dros y ffôn: deialwch 18001 ac yna 0345 300 3900. Sylwer – dim ond galwadau ffôn Saesneg eu hiaith y mae Relay UK yn gallu ymdrin â nhw.
+          |Os ydych y tu allan i’r DU: +44 300 200 1900.
           |Cyn i chi ffonio, sicrhewch fod gennych y canlynol:
+          |eich Cyfeirnod Unigryw y Trethdalwr (UTR) 10 digid
           |gwybodaeth am unrhyw gynilion neu fuddsoddiadau sydd gennych
           |eich manylion banc
-          |Rydym yn debygol o ofyn:
+          |manylion eich incwm a’ch gwariant
+          |Rydym yn debygol o ofyn y canlynol:
           |beth rydych wedi’i wneud i geisio talu’r bil
           |a allwch dalu rhywfaint o’r bil nawr
-          |Ein horiau agor yw:
-          |Dydd Llun i ddydd Gwener: 08:30 i 17:00 (rydym ar gau ar benwythnosau a gwyliau banc)
         """.stripMargin
     }
 
