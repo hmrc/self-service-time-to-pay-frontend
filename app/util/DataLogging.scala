e51b1dabbc1f6a27d910deef45fbe8fcc91cf50b
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-08-23 15:29:15 +0100

    [AM][OPS-10831] add data logging object

--- app/util/DataLogging.scala
+++ app/util/DataLogging.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package util
 
@@ -24,8 +39,8 @@ object DataLogging {
       lazy val richMessageWithArrangement = makeRichMessage(message) + appendedArrangement(arrangement)
       level match {
         case Debug => log.debug(richMessageWithArrangement)
-        case Info => log.info(richMessageWithArrangement)
-        case Warn => log.warn(richMessageWithArrangement)
+        case Info  => log.info(richMessageWithArrangement)
+        case Warn  => log.warn(richMessageWithArrangement)
         case Error => log.error(richMessageWithArrangement)
       }
     }
@@ -48,8 +63,8 @@ object DataLogging {
       lazy val richMessageWithArrangement = makeRichMessage(message) + appendedSubmissionError(submissionError)
       level match {
         case Debug => log.debug(richMessageWithArrangement)
-        case Info => log.info(richMessageWithArrangement)
-        case Warn => log.warn(richMessageWithArrangement)
+        case Info  => log.info(richMessageWithArrangement)
+        case Warn  => log.warn(richMessageWithArrangement)
         case Error => log.error(richMessageWithArrangement)
       }
     }
@@ -72,8 +87,8 @@ object DataLogging {
       lazy val richMessageWithArrangement = makeRichMessage(message) + appendedPaymentPlanRequest(paymentPlanRequest)
       level match {
         case Debug => log.debug(richMessageWithArrangement)
-        case Info => log.info(richMessageWithArrangement)
-        case Warn => log.warn(richMessageWithArrangement)
+        case Info  => log.info(richMessageWithArrangement)
+        case Warn  => log.warn(richMessageWithArrangement)
         case Error => log.error(richMessageWithArrangement)
       }
     }
