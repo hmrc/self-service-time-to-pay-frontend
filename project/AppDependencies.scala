1896a41840c08f31530e07562435b46a1997506d
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-10-02 13:18:44 +0100

    [AM][OPS-9551] update bootstrap-frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"           % "0.48.0",
     "uk.gov.hmrc"         %% "play-frontend-hmrc"                 % "7.21.0-play-28",
-    "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"         % "7.15.0",
+    "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"         % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-28"                 % "1.3.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"      % "1.13.0-play-28",
     "uk.gov.hmrc"         %% "time"                               % "3.25.0",   // brakes on newer versions
