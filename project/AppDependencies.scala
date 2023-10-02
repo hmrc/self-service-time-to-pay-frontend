02ca6fe8d599205f35d82309ad7b1f559e3a181a
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-10-02 12:16:04 +0100

    [AM][OPS-9551] update hmrc-mongo-play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,35 +2,30 @@ import play.sbt.PlayImport.ws
 import sbt._
 
 object AppDependencies {
+
+  val boostrapVersion = "7.15.0"
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.15.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.19.0-play-28",
-
-    "uk.gov.hmrc" %%  "play-conditional-form-mapping" % "1.12.0-play-28",
-
-    "uk.gov.hmrc" %% "time" % "3.25.0",   // brakes on newer versions
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.1.0",
-
-    "uk.gov.hmrc" %% "time-to-pay-taxpayer-cor" % "0.48.0",
-
-    "uk.gov.hmrc" %% "domain" %  "8.1.0-play-28",
-    "com.typesafe.play" %% "play-json-joda" % "2.9.4",
-
-    "com.beachape" %% "enumeratum-play" % "1.7.0",  // upgrade to 1.7.2 is not binary compatable
-    "org.julienrf" %% "play-json-derived-codecs" % "10.1.0",
-
-    "org.typelevel" %% "cats-core" % "2.9.0",
-
+    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"           % "0.48.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"                 % "7.19.0-play-28",
+    "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"         % "7.15.0",
+    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-28"                 % "1.3.0",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping"     % "1.12.0-play-28",
+    "uk.gov.hmrc"         %% "time"                               % "3.25.0",   // brakes on newer versions
+    "uk.gov.hmrc"         %% "domain"                             % "8.1.0-play-28",
+    "com.typesafe.play"   %% "play-json-joda"                     % "2.9.4",
+    "com.beachape"        %% "enumeratum-play"                    % "1.7.0",  // upgrade to 1.7.2 is not binary compatable
+    "org.julienrf"        %% "play-json-derived-codecs"           % "10.1.0",
+    "org.typelevel"       %% "cats-core"                          % "2.9.0",
   )
 
   val test = Seq(
-    "com.github.tomakehurst" % "wiremock-standalone" % "2.27.2" % "test",
-    "org.scalatest" %% "scalatest" % "3.2.15" % "test",
-    "com.vladsch.flexmark" %  "flexmark-all" % "0.62.2" % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test",
-    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % "test",
-    "com.softwaremill.macwire" %% "macros" % "2.5.8" % "test",
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.15.0" % "test"
-  )
+    "com.github.tomakehurst"      % "wiremock-standalone"         % "2.27.2",
+    "org.scalatest"               %% "scalatest"                  % "3.2.15",
+    "com.vladsch.flexmark"        %  "flexmark-all"               % "0.62.2",
+    "org.scalatestplus.play"      %% "scalatestplus-play"         % "5.1.0",
+    "org.scalatestplus"           %% "scalacheck-1-15"            % "3.2.11.0",
+    "com.softwaremill.macwire"    %% "macros"                     % "2.5.8",
+    "uk.gov.hmrc"                 %% "bootstrap-test-play-28"     % boostrapVersion
+  ).map(_ % Test)
 }
