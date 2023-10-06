5888d183c0683d942a1b9dff415ad90ae557a59f
tp10 <1626193+tp10@users.noreply.github.com>
2023-10-06 08:56:18 +0100

    [TP][OPS-11012] - Remove Fieldset tags - update dependancies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"           % "0.57.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"                 % "7.21.0-play-28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"                 % "7.22.0-play-28",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"         % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-28"                 % "1.3.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"      % "1.13.0-play-28",
