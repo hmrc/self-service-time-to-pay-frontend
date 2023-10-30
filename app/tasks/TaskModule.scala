5377be5b764602d4b6438aaaa0f79ba652d007b1
Kostas Ntanos <kostas@ntanos.co.uk>
2023-10-30 14:03:53 +0000

    [KN][OPS-9009] Drop old mongo DB (#884)

---
+++ app/tasks/TaskModule.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tasks
+
+import play.api.Logging
+import play.api.inject._
+import uk.gov.hmrc.mongo.MongoComponent
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+class TasksModule extends SimpleModule(bind[CleanupTask].toSelf.eagerly())
+
+@Singleton
+class CleanupTask @Inject() (mongoComponent: MongoComponent)(implicit ec: ExecutionContext) extends Logging {
+  logger.info("**************** Start cleanup tasks...")
+
+  mongoComponent.client
+    .getDatabase("self-service-time-to-pay-frontend") // update
+    .getCollection("journey") // update
+    .drop()
+    .toFuture()
+    .map { _ => logger.info("**************** cleanup done.") }
+}
