de168601f8eb02cb65362f62da0b5f32c7112781
tp10 <1626193+tp10@users.noreply.github.com>
2024-04-17 10:10:27 +0100

    [TP][OPS-12026] - Add English label

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,20 +10,20 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.61.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "9.4.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "9.6.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "1.8.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "2.0.0",
     "uk.gov.hmrc"         %% "domain-play-30"                        % "9.0.0",
     "uk.gov.hmrc"         %% "crypto-json-play-30"                   % cryptoVersion,
     "com.beachape"        %% "enumeratum-play"                       % "1.8.0",
-    "org.julienrf"        %% "play-json-derived-codecs"              % "10.1.0",
+    "org.julienrf"        %% "play-json-derived-codecs"              % "11.0.0",
     "org.typelevel"       %% "cats-core"                             % "2.10.0"
   )
 
   val test = Seq(
     "org.scalatest"               %% "scalatest"                  % "3.2.18",
-    "org.wiremock"                % "wiremock-standalone"         % "3.4.2",
+    "org.wiremock"                % "wiremock-standalone"         % "3.5.2",
     "org.scalatestplus.play"      %% "scalatestplus-play"         % "7.0.1",
     "org.scalatestplus"           %% "scalacheck-1-15"            % "3.2.11.0",
     "com.softwaremill.macwire"    %% "macros"                     % "2.5.9",
