9b05b9f134c030f10a9fe30f259a125cce4b3ee7
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2024-04-25 14:11:00 +0100

    [KN][OPS-11798] More dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,8 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.62.0-SNAPSHOT",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "9.9.0",
+    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.62.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "9.10.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "1.9.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "2.0.0",
