42eac2222f2561c73cd146344860a69522043e47
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2024-07-04 10:38:15 +0100

    [KN][OPS-12362] Dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "10.3.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.1.0",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "2.0.0",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.0.0",
     "uk.gov.hmrc"         %% "domain-play-30"                        % "10.0.0",
     "uk.gov.hmrc"         %% "crypto-json-play-30"                   % cryptoVersion,
     "com.beachape"        %% "enumeratum-play"                       % "1.8.0",
