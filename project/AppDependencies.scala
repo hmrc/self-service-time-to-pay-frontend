e7f5fbf946e42026d583c751d4c5d6360c5878aa
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-08-29 14:30:40 +0100

    [JG] OPS-12665 - dependency upgrades (#916)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,16 +4,16 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "9.3.0"
+  val boostrapVersion = "9.4.0"
   val cryptoVersion = "8.0.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.63.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "10.9.0",
+    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.64.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "10.10.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.2.0",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.1.0",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.2.0",
     "uk.gov.hmrc"         %% "domain-play-30"                        % "10.0.0",
     "uk.gov.hmrc"         %% "crypto-json-play-30"                   % cryptoVersion,
     "com.beachape"        %% "enumeratum-play"                       % "1.8.0",
