666440bac9642a0ed0bcf511d4cc2e9b8c831f2f
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2025-05-19 09:53:17 +0100

    [JG] OPS-13647 - interest rate change (#928)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "9.11.0"
+  val boostrapVersion = "9.12.0"
   val cryptoVersion = "8.2.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.67.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.0.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.1.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.6.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.3.0",
