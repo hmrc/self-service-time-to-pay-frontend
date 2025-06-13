572fb9fa337a4ea4a2ebdeee2d923eb2ee81ee28
Kntanos <kostas@ntanos.co.uk>
2025-06-13 11:13:53 +0100

    [KN][OPS-13717] Dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,8 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.67.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.5.0",
+    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.68.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.6.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.6.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.3.0",
