64ed42efa24c4abb0fc7023e4b000565257925db
Kostas Ntanos <kostas@ntanos.co.uk>
2025-06-13 11:35:19 +0100

    [KN][OPS-13717] Label O11 microservices for the catalogue (#930)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "9.12.0"
+  val boostrapVersion = "9.13.0"
   val cryptoVersion = "8.2.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.67.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.1.0",
+    "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.68.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.6.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.6.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.3.0",
@@ -23,7 +23,7 @@ object AppDependencies {
 
   val test = Seq(
     "org.scalatest"               %% "scalatest"                  % "3.2.19",
-    "org.wiremock"                % "wiremock-standalone"         % "3.13.0",
+    "org.wiremock"                % "wiremock-standalone"         % "3.13.1",
     "org.scalatestplus.play"      %% "scalatestplus-play"         % "7.0.1",
     "org.scalatestplus"           %% "scalacheck-1-15"            % "3.2.11.0",
     "com.softwaremill.macwire"    %% "macros"                     % "2.6.6",
