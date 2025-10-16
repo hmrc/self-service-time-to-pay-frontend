edb2cc339e92a6395e465680820051ab7232870a
Kostas Ntanos <kostas@ntanos.co.uk>
2025-10-16 15:30:06 +0100

    [KN][OPS-14204] Increase journey.ttl (#935)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,15 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "10.1.0"
-  val cryptoVersion = "8.3.0"
+  val boostrapVersion = "10.3.0"
+  val cryptoVersion = "8.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.69.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.12.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.18.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
-    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.7.0",
+    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.10.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"         %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc"         %% "crypto-json-play-30"                   % cryptoVersion,
