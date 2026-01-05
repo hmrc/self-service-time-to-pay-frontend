b7758c4d4d60be9e5cea08b5c1592359c8e6c103
Kntanos <kostas@ntanos.co.uk>
2026-01-05 10:15:56 +0000

    [KN][OPS-14466] Interest rate change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,16 +4,16 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "10.3.0"
+  val boostrapVersion = "10.5.0"
   val cryptoVersion = "8.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"         %% "time-to-pay-taxpayer-cor"              % "0.69.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.18.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"            % "12.25.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"            % boostrapVersion,
-    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.10.0",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                    % "2.11.0",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"         %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc"         %% "crypto-json-play-30"                   % cryptoVersion,
     "com.beachape"        %% "enumeratum-play"                       % "1.9.0",
@@ -23,7 +23,7 @@ object AppDependencies {
 
   val test = Seq(
     "org.scalatest"               %% "scalatest"                  % "3.2.19",
-    "org.wiremock"                % "wiremock-standalone"         % "3.13.1",
+    "org.wiremock"                % "wiremock-standalone"         % "3.13.2",
     "org.scalatestplus.play"      %% "scalatestplus-play"         % "7.0.2",
     "org.scalatestplus"           %% "scalacheck-1-15"            % "3.2.11.0",
     "com.softwaremill.macwire"    %% "macros"                     % "2.6.7",
